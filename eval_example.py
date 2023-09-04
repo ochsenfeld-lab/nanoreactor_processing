d3e14cbbfbabff28eedc7684badac4440f03fa2a
lglinkina <glinkina.liubov@gmail.com>
2023-09-04 11:23:43 +0200

    added sulfur and radicals, created nanolysator executable for reactions evaluation

--- eval_example.py
+++ eval_example.py
@@ -16,16 +16,19 @@ nanoSim = NanoSim(sys.argv[1], sys.argv[2])
 # comment next row if you use second type of initialization
 nanoSim.generate_frag_lists()
 
-# generate SMILES and fill in data frame
-nanoSim.generate_mols_smiles()
+# generate SMILES and fill in data frame; specify the presence of radicals
+nanoSim.generate_mols_smiles(radicals=False)
 nanoSim.generate_df()
 
 # plot mol grid and bar plot
 plot_tools.generate_mol_grid(nanoSim.df)
 plot_tools.generate_bar_plot(nanoSim.df)
 
+# calc start_ts_index as (equil_time%period_time) / (dt * read_rate) + 475 fs (19th time step)
+# here 40 + 19 for 5000 fs equilibration/heating time
+
 # make reactions list and network
-reactions_list = nanoreactor_network.construct_reactions_list(nanoSim.df)
+reactions_list = nanoreactor_network.construct_reactions_list(nanoSim.df,start_ts_index=59)
 
 if reactions_list==[]:
     print('No events found!')
